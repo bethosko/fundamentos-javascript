d522148e138aaf892b99c824554f565f7397452a
Humberto <bethosko@gmail.com>
2025-05-08 13:59:20 -0300

    loop exercicio

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("olá")
+
+ 
+/*let opcoes = Number(prompt(`
+    Selecione a opção desejada:
+    1 - Saque
+    2 - transferencia
+    3 - Saldo
+    4 - Extrato
+    `));
+
+switch (opcoes){
+    case 1:
+        alert("Saque");
+        break;
+    case 2:
+        alert("Transferencia");
+        break;
+    case 3:
+        alert("Saldo");
+        break;
+    case 4:
+        alert("Extrato");
+        break;
+    default:
+        alert("Opção inválida");
+       
+}*/
+
+/*for (let i = 1; i <= 10; i++) {
+    console.log(`passo: ${i}`);
+}*/
+
+for (let i = 2; i <= 20; i = i + 2) {
+    console.log(`passo: ${i}`);
+}
