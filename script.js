3a755105c85d24e0ecc89248c43ba30703c05ef7
Humberto <bethosko@gmail.com>
2025-05-08 14:50:45 -0300

    maismais exercicio de loop

--- script.js
+++ script.js
@@ -62,3 +62,9 @@ for (let i = 1; i <= 10; i++) {
  }
 */
 
+let numero = Number (prompt(" Digite um numero: "));
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+    
+}
